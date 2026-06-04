f1856309e09fb114fbcb2dfa1fc748dfb4f589b4
nogi2466-tech <nogi2466@gmail.com>
2026-06-05 03:12:18 +0900

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,5 +1,5 @@
 // ⚠️ 以下のURLをご自身の最新のGASウェブアプリURL（末尾が/execのもの）に書き換えてください
-const GAS_URL = "https://script.google.com/macros/s/AKfycbxEi14YSuM1zu0E2HdxYgM2cE4lUY8vkZf5fRZ2lDbjVu96GlprJn5N6U2oYBnFi4XpjQ/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbyAhr1G0BaFOrvb8S-vWR01mySEUQGdu-2_d-Fk7oav7gyBv-OxUrlatBb6qIx6Plwc/exec";
 const MASTER_PASS = "0829"; 
 let links = JSON.parse(localStorage.getItem('tetsudo_links')) || [];
 let isUnlocked = false;
